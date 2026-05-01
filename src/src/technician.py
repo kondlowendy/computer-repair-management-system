a40f9c61bd22bfdc653b07783196121b4b63ac7c
kondlowendy <110423288+kondlowendy@users.noreply.github.com>
2026-05-01 22:42:47 +0200

    Delete src/src directory

--- src/src/technician.py
+++
@@ -1,8 +0,0 @@
-class Technician:
-    def __init__(self, tech_id, name, specialization):
-        self.tech_id = tech_id
-        self.name = name
-        self.specialization = specialization
-
-    def assign_job(self, job):
-        job.technician = self
